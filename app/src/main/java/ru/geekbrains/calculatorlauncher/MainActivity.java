35b12801d2e9f06871c6d44fc8c34ce909b6da36
PivIgor <brauer.igor@gmail.com>
2021-06-06 23:16:48 +0300

    Init commit.

---
+++ app/src/main/java/ru/geekbrains/calculatorlauncher/MainActivity.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.calculatorlauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
